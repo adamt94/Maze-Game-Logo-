7a7c5a1f6d9eb67cbdab9166fff2e3a0b22bbabf
unknown <ypf12pxu@CMPLAB3-11.UEA.AC.UK>
2016-05-26 17:22:09 +0100

    fixed boolean value

--- turtle.js
+++ turtle.js
@@ -384,7 +384,7 @@ Player.prototype.backward = function (d) {
 Player.prototype.right = function (angle) {
     this.saveState(this.x,this.y,this.angle);
 	
-	if(angle%90!==0 && this.gamemode == false)
+	if(angle%90!==0 && this.gamemode == true)
 	{
 	 oldcode.innerHTML += "\n" + "error :You can only rotate at 90 degree angles!";
 	}else{
